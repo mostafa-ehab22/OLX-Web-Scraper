862a156e425af5236c3fd99b321494877a00cca6
Mostafa Ehab <mostafaehab0002@gmail.com>
2025-11-30 00:14:08 +0200

    Fix formatting of color code constants

--- project.py
+++ project.py
@@ -12,13 +12,13 @@ OLX Web Scraper Project
 
 # ANSI color codes for enhanced printing
 class Colors:
-    HEADER = '\033[95m'  # Yellow
+    HEADER = '\033[95m'  
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
-    RED = '\033[91m'  # Red
+    RED = '\033[91m'  
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
